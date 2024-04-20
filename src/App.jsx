9ff1deb97b88f2df4d7014eddb9ca4e241a434e6
merv-e <34655454+merv-e@users.noreply.github.com>
2024-04-20 16:32:12 +0300

    - some minor changes are made, so that the counter can be increased or decreased by a random value in an input.
    - some minor changes are made in style as well.
    
    - In addition to those typos are fixed in counterSlice.

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,86 @@
 import { useDispatch, useSelector } from "react-redux";
 import {
-  decrement,
-  increaseBy10,
   increment,
+  decrement,
   toggle,
+  increaseBy,
+  decreaseBy,
 } from "./features/counterSlice";
+import { useState } from "react";
 
 function App() {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter.value);
   const isCounterShown = useSelector((state) => state.counter.isCounterShown);
+
+  const [changeValue, setChangeValue] = useState(0);
+
+  const [isBtnDisabled, setIsBtnDisabled] = useState(true);
+
+  const handleInputChange = (e) => {
+    setChangeValue(parseInt(e.target.value));
+    if (changeValue && changeValue !== "" && changeValue !== 0) {
+      setIsBtnDisabled(false);
+    } 
+    else setIsBtnDisabled(true)
+  };
+
   return (
     <div>
       <h1>Basic Counter App</h1>
       <div className="counter">
         <p
-          className={`counter-value ${isCounterShown ? "counter-visible" : "counter-invisible"}`}
+          className={`counter-value ${
+            isCounterShown ? "counter-visible" : "counter-invisible"
+          }`}
         >
           Counter : {counter}
         </p>
-        <div className="counter-btn-container">
-          <button
-            className="btn-container"
-            onClick={() => dispatch(increment())}
-          >
-            Increment
-          </button>
-          <button
-            className="btn-container"
-            onClick={() => dispatch(decrement())}
-          >
-            Decrement
-          </button>
-          <button className="btn-container" onClick={() => dispatch(toggle())}>
-            Toggle
-          </button>
-          <button
-            className="btn-container"
-            onClick={() => dispatch(increaseBy10(10))}
-          >
-            Increase By 10
-          </button>
+        <div className="counter-btn-containers">
+          <div className="counter-btn-container">
+            <button className="btn" onClick={() => dispatch(increment())}>
+              Increment
+            </button>
+            <button className="btn" onClick={() => dispatch(decrement())}>
+              Decrement
+            </button>
+            <button className="btn" onClick={() => dispatch(toggle())}>
+              Toggle
+            </button>
+          </div>
+
+          <div className="counter-btn-container">
+            <label htmlFor="counter-value">
+              Increase Or Decrease the Counter
+            </label>
+            <input
+              type="number"
+              id="counter-value"
+              placeholder="Please write a number"
+              value={changeValue}
+              onChange={handleInputChange}
+            />
+          </div>
+
+          <div className="counter-btn-container">
+            <button
+              className={isBtnDisabled ? "disabled-btn" : "btn"}
+              disabled={!changeValue}
+              onClick={() => dispatch(increaseBy(parseInt(changeValue)))}
+            >
+              Increase By
+              {changeValue !== 0 && changeValue ? changeValue : " .."}
+            </button>
+
+            <button
+              className={isBtnDisabled ? "disabled-btn" : "btn"}
+              disabled={!changeValue}
+              onClick={() => dispatch(decreaseBy(parseInt(changeValue)))}
+            >
+              Decrease By
+              {changeValue !== 0 && changeValue ? changeValue : " .."}
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- src/features/counterSlice.js
+++ src/features/counterSlice.js
@@ -1,26 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const counterSlice = createSlice({
-    name: "counter",
-    initialState: {
-        value: 0,
-        isCounterShown : true 
+  name: "counter",
+  initialState: {
+    value: 0,
+    isCounterShown: true,
+  },
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
     },
-    reducers : {
-        increment : (state) => {
-            state.value += 1
-        },
-        decrement : (state) => {
-            state.value -= 1
-        },
-        toggle : (state) => {
-            state.isCounterShown = !state.isCounterShown
-        },
-        increaseBy10 : (state, action) => {
-            state.value += action.payload
-        },
-    }
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    toggle: (state) => {
+      state.isCounterShown = !state.isCounterShown;
+    },
+    increaseBy: (state, action) => {
+      state.value += action.payload;
+    },
+    decreaseBy: (state, action) => {
+      state.value -= action.payload;
+    },
+  },
 });
 
-export const {increment, decrement, toggle, increaseBy10} = counterSlice.actions;
+export const { increment, decrement, toggle, increaseBy, decreaseBy } =
+  counterSlice.actions;
 export default counterSlice.reducer;
